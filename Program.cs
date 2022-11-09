02666a68f717aa643b45ff70eeb7734d2a062d93
NeivaYang <yangaraujo.neiva@gmail.com>
2022-11-08 23:29:04 -0300

    End Console  App

--- Program.cs
+++ Program.cs
@@ -14,15 +14,24 @@ namespace koniaE1
             try
             {
                 sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+                Console.WriteLine("Success");
+                List<listaNomes> lista = Nomes.GetlistaNomes();
+                lista.ForEach(ln => Console.WriteLine ("ID: "+ln.Id+ ", Nome: " +ln.Nome));
+                Console.WriteLine("Informe qual nome da lista deseja inserir");
+                string name = Console.ReadLine();
+                int ID = int.Parse(Console.ReadLine());
+                DateTime data = new DateTime.Now;
+                string insertQuery = "INSERT INTO DETAILS(ID, NOME, DATA_CREACAO) VALUES("+ ID +", "+ name +", "+ data +")" ;
+                SqlCommand insertCommand = new SqlCommand(insertQuery, sqlConnection);
+                insertCommand.ExecuteNonQuery();
+                Console.WriteLine("Insert Succesfully");
+                sqlConnection.Close();
             }
             catch(Exception e)
             {
-                Console.WriteLine(e.Message)
+                Console.WriteLine(e.Message);
             }
-            List<listaNomes> lista = Nomes.GetlistaNomes();
-            lista.ForEach(ln => Console.WriteLine ("ID: "+ln.Id+ ", Nome: " +ln.Nome));
-            Console.WriteLine("Informe qual nome da lista deseja inserir(Informe o id correspondente ao nome): ");
-            Console.ReadKey();
         }
     }
 }
